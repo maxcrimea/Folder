961b8cd6cbc6d13eac64018876ca33fb14b6eb01
maxcrimea <maxcrimea50@gmail.com>
2023-01-23 02:33:52 +0600

    he_6

---
+++ home2/main.py
@@ -0,0 +1,5 @@
+from colorama import Fore, Back, Style
+print(Fore.GREEN + 'зеленый текст')
+print(Back.YELLOW + 'на желтом фоне')
+print(Style.BRIGHT + 'стал ярче' + Style.RESET_ALL)
+print('обычный текст')
